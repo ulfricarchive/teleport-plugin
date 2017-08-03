5e1efeef6d6e99b31de29a4f5ccec9820252be9c
Adam Harrison <ad@mharrison.us>
2017-08-02 22:16:08 -0400

    Container cleanup

--- src/main/java/com/ulfric/scotty/Scotty.java
+++ src/main/java/com/ulfric/scotty/Scotty.java
@@ -1,11 +1,18 @@
 package com.ulfric.scotty;
 
 import com.ulfric.platform.Plugin;
+import com.ulfric.scotty.command.TeleportCommand;
+import com.ulfric.scotty.command.TeleportHereCommand;
+import com.ulfric.scotty.command.TeleportHereNowCommand;
 
 public class Scotty extends Plugin {
 
 	public Scotty() {
-		install(ScottyContainer.class);
+		install(Teleport.class);
+
+		install(TeleportCommand.class);
+		install(TeleportHereCommand.class);
+		install(TeleportHereNowCommand.class);
 	}
 
 }
